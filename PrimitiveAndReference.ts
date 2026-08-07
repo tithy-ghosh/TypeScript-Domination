5243ffdff54a1e1fb7c37f0628f76a975986d879
tithy-ghosh <sgt.tithy.717@gmail.com>
2026-08-08 00:55:12 +0600

    Refactor PrimitiveAndReference.ts to include additional examples of primitive and reference types

--- PrimitiveAndReference.ts
+++ PrimitiveAndReference.ts
@@ -6,3 +6,16 @@ var a = true  //primitive
 var b = [1, 2, 3, 4] //Reference
 
 var c = b //The value of c is not equal to the value of b. In the c goes a reference of b.
+
+// Number
+let x = 12
+let y = 12.232
+// x = 'tithy' // You cant assign a new types in same variable. It will show ts error
+// String
+ let s = 'tithy'
+//  Boolean
+let t  = true
+
+// Array
+
+let arr = [1, 2, 3];

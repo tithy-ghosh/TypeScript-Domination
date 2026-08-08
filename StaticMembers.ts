84323d25c9f8ab9708323bf00d167c736198da66
tithy-ghosh <sgt.tithy.717@gmail.com>
2026-08-09 00:31:25 +0600

    Add StaticMembers.ts with examples of static properties and methods in classes

---
+++ StaticMembers.ts
@@ -0,0 +1,14 @@
+// If we declare any property by static it will belongs to only class not the instance. So there need not to be create extra instance for staticmembers.
+
+class Founder{
+    static name: string = 'Tithy'// It can be accessed directly without creating instance
+
+}
+
+class RandomNumber{
+    static getRandomNumber(): number{
+        return Math.random()
+    }
+}
+
+const founder1 = new Founder() //It will not contain static members

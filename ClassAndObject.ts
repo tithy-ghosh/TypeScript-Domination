165cc0e61c30a4763379e2c8533b6d78ced46e53
tithy-ghosh <sgt.tithy.717@gmail.com>
2026-08-08 18:02:53 +0600

    Add ClassAndObject.ts with a Device class definition and example instantiation

---
+++ ClassAndObject.ts
@@ -0,0 +1,11 @@
+//Defining class
+
+class Device{
+    name = 'Infinix Smart 9';
+    price = 11600;
+    category = 'Smart Phone'
+}
+const d1 = new Device();
+
+const d2 = new Device();
+//This will generate two new device but the value of d1 and d2 will be same. To pass the different value in d1 and d2 we have to take help from constructor
